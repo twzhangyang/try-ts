6f237ee3ecd3f841a08f0b42daf49afc2a4068af
yang <yrzhang@thoughtworks.com>
2020-06-14 20:26:59 +0800

    router

--- fc/src/Post.js
+++ fc/src/Post.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {useParams, useLocation, useHistory} from 'react-router';
 
 function Post(props) {
   useEffect(()=>{
@@ -18,12 +19,24 @@ function Post(props) {
     console.log(item);
   }
 
+  const id2 = useParams();
+  console.log(id2);
+  const location = useLocation();
+  console.log(location);
+  const history = useHistory();
+  console.log(history);
+
+  const goBack = () => {
+    history.goBack();
+  }
+
   return (
     <div className="App">
       Post page
       <h1>{item.id}</h1>
       <p>{item.title}</p>
       <p>{item.body}</p>
+      <button onClick={goBack}>Go back</button>
     </div>
   );
 }
