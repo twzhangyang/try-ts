02f3adc78b0ce7af2c1b55d109bed02f98596adf
yang <yrzhang@thoughtworks.com>
2020-06-14 19:48:04 +0800

    lost

--- fc/src/Nav.js
+++ fc/src/Nav.js
@@ -13,7 +13,7 @@ function Nav() {
   return (
     <nav>
       <h3>Logo</h3>
-      <ul className="nav-links">
+      <ul className="nav-links"> 
         <Link style={navStyle} to="/"> <li>Home</li> </Link>
         <Link style={navStyle} to="/about"> <li>About</li> </Link>
         <Link style={navStyle} to="/shop"> <li>Shop</li> </Link>
