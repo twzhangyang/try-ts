e5cacc01a0d3a233b8df8e4a37de03b9859dd148
Yang.Zhang <yang.zhang@rea-group.com>
2020-07-01 23:43:00 +0800

    html-loader & file-loader

--- webpack-demo-app/webpack.common.js
+++ webpack-demo-app/webpack.common.js
@@ -2,20 +2,32 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  plugins:[new HtmlWebpackPlugin({
-    template: "./src/template.html"
-  })]
+    entry: "./src/index.js",
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(svg|png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: "[name].[hash].[ext]",
+                    outputPath: 'images',
+                },
+            }
+        ]
+    }
 }
