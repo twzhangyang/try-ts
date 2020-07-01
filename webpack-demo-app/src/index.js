cd01ef01e2202e28312471bb9cc8e63052c66740
Yang.Zhang <yang.zhang@rea-group.com>
2020-07-01 22:12:19 +0800

    add sass-loader

--- webpack-demo-app/src/index.js
+++ webpack-demo-app/src/index.js
@@ -2,6 +2,7 @@ import {AlertService} from './app/alert-service';
 import {ComponentService} from './app/component-service';
 import {run} from './app/app';
 import './main.css';
+import './mainsass.scss';
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
