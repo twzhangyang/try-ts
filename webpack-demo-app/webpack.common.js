5e48747d167bcd1bf8ddd3d8864fe6ea0aa948c7
Yang.Zhang <yang.zhang@rea-group.com>
2020-07-01 23:50:21 +0800

    clean-webpack-plugin

--- webpack-demo-app/webpack.common.js
+++ webpack-demo-app/webpack.common.js
@@ -1,11 +1,14 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
     plugins: [new HtmlWebpackPlugin({
         template: "./src/template.html"
-    })],
+    }),
+    new CleanWebpackPlugin()
+    ],
     module: {
         rules: [
             {
