e7cc65a3c35265027e18a1148bdabc3330300a6c
Yang.Zhang <yang.zhang@rea-group.com>
2020-07-01 23:06:10 +0800

    webpack dev&prod&dev-server

--- webpack-demo-app/webpack.common.js
+++ webpack-demo-app/webpack.common.js
@@ -2,13 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode:"development",
-  devtool: "none",
   entry: "./src/index.js",
-  output: {
-    filename: "main-[contentHash].js",
-    path: path.resolve(__dirname, "dist")
-  },
   module: {
     rules: [
       {
