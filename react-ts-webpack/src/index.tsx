7ae366332bd6a5f06734d147e58186e0a2f62f22
Yang.Zhang <yang.zhang@rea-group.com>
2020-07-05 23:15:06 +0800

    useState

--- react-ts-webpack/src/index.tsx
+++ react-ts-webpack/src/index.tsx
@@ -1,21 +1,41 @@
 import React, { Fragment, useState } from 'react';
 import ReactDOM from 'react-dom';
 
+type FormElem = React.FormEvent<HTMLFormElement>
+
+interface ITodo {
+  text: string
+  complete: boolean
+}
+
 export const App = (): JSX.Element => {
   const sum = (a: number, b: number): number => {
     return a + b;
   }
 
-  const [value, setValue] = useState('');
-  debugger
+  const [value, setValue] = useState<string>('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElem) => {
+    e.preventDefault();
+    addTodo(value);
+    setValue('');
+  }
+
+  const addTodo = (text: string) => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  }
+
+  console.log(todos);
 
   return (
     <Fragment>
       Hello
       {sum(6, 3)}
       <h1> Todo list</h1>
-      <form>
-        <input type='text' required/>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={value} onChange={e => setValue(e.target.value)} required/>
         <button type='submit'> Add Todo</button>
       </form>
     </Fragment>
