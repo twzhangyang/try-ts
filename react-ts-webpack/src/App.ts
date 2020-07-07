ab25489bd48173561e9e1e91b89889f018d772ba
Yang.Zhang <yang.zhang@rea-group.com>
2020-07-07 22:20:59 +0800

    rename to react-fc

---
+++ react-ts-webpack/src/App.ts
@@ -0,0 +1,12 @@
+import React, { Provider } from 'react';
+
+const {Provider, Consumer} = React.createContext({});
+const Parent = () => {
+ const text = "random text";
+
+ return
+}
+
+const Child = () => {
+
+}
