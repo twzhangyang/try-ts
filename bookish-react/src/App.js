a79c21c201eb9d71e3f454a9941c45c75de7a228
Yang.Zhang <yang.zhang@rea-group.com>
2020-07-21 20:55:53 +0800

    setup bookish

---
+++ bookish-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from "@material-ui/core/Typography";
+
+function App() {
+  return (
+    <div>
+      <Typography>Hello world</Typography>
+    </div>
+  );
+}
+
+export default App;
